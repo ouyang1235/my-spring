cb5e5869e602b9b5f424185f60f8f77ccb3487f5
欧阳宇轩 <365261277@qq.com>
2021-11-18 21:39:53 +0800

    step-05

--- step-05/src/main/java/cn/bugstack/springframework/beans/factory/xml/XmlBeanDefinitionReader.java
+++ step-05/src/main/java/cn/bugstack/springframework/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -1,4 +1,50 @@
 package cn.bugstack.springframework.beans.factory.xml;
 
-public class XmlBeanDefinitionReader {
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.support.AbstractBeanDefinitionReader;
+import cn.bugstack.springframework.beans.factory.support.BeanDefinitionRegistry;
+import cn.bugstack.springframework.core.io.Resource;
+import cn.bugstack.springframework.core.io.ResourceLoader;
+import cn.hutool.core.util.XmlUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
+    protected XmlBeanDefinitionReader(BeanDefinitionRegistry registry) {
+        super(registry);
+    }
+
+    public XmlBeanDefinitionReader(BeanDefinitionRegistry registry, ResourceLoader resourceLoader) {
+        super(registry, resourceLoader);
+    }
+
+    @Override
+    public void loadBeanDefinitions(Resource resource) throws BeansException {
+        try {
+            try (InputStream inputStream = resource.getInputStream()){
+
+            }
+        }catch (IOException e){
+
+        }
+    }
+
+    @Override
+    public void loadBeanDefinitions(Resource... resource) throws BeansException {
+
+    }
+
+    @Override
+    public void loadBeanDefinitions(String location) throws BeansException {
+
+    }
+
+    protected void doLoadBeanDefinitions(InputStream inputStream) throws ClassNotFoundException {
+        Document document = XmlUtil.readXML(inputStream);
+        Element root = document.getDocumentElement();
+        root.getChi
+    }
 }
